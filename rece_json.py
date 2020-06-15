9ee955c76bff54ddfb33ff2be0d8a2c8a88f65cb
OkamuraMasaki <tukuoka7@gmail.com>
2020-06-15 17:58:15 +0900

    commit about api no.2

--- rece_json.py
+++ rece_json.py
@@ -8,34 +8,81 @@ import json
 def rec_1():
     #jsonデータを受け取る.
     rec_data_1 = request.json["tags"]
+    rec_data_2=request.json["questions"]
     teachers=rec_data_1
+    #print(rec_data_2)
     print(rec_data_1)
-    print(request.method)
+    #print(request.method)
     #ques=rec_data_1[1]
     #-------------------計算part1
     conn = sqlite3.connect("finder.db")
     c = conn.cursor()
     teachers_list = []
-    for tea in teachers:
+    for tea in rec_data_1:
         sel = "select distinct name from teacher where id=?"
         data=tea
 
-        c.execute(sel,str(data))
+        c.execute(sel,str(tea))
         for row in c.fetchall():
             teachers_list.append(row)
 
     conn.close()
     #-----part2
+    output_arr=[]
+    teacher_set={}
+    def cal(arr1,arr2):
+        sum_arr=[]
+        sum_arr.append((arr1[0]-arr2[0])**2)
+        sum_arr.append((arr1[1]-arr2[1])**2)
+        sum_arr.append((arr1[2]-arr2[2])**2)
+        q_sum=sum(sum_arr)
+
+        return q_sum
+
     for tea in teachers_list:
+        conn = sqlite3.connect("finder.db")
+        c = conn.cursor()
+        select = "select zemitime,com,seat from facility where name=?"
+        data=tea
+        #print(tea)
+        c.execute(select,(str(tea[0]),))
+        for row in c.fetchall():
+            comp=cal(rec_data_2,list(row))
+            #print(row)
+            teacher_set[tea]=comp
+            #output_arr.append(teacher_set)
 
-        print(tea[0])
+        conn.close()
+    #print(teacher_set)
+    cri_lab=sorted(teacher_set.items(),reverse=True)
+    output_json=[]
+    #print(cri_lab)
+    for tea in cri_lab:
+        conn = sqlite3.connect("finder.db")
+        c = conn.cursor()
+        select = "select name,outline,need,osusume from explanation where name=?"
+        #print(tea[0][0])
+        c.execute(select,(str(tea[0][0]),))
+        for row in c.fetchall():
+            #print(row)
+            output_json.append({
+                "name":row[0],
+                "options":[{
+                    "outline":row[1],
+                    "need":row[2]
+                }]
+            })
 
-
-    output_1 = {"message": "OK"}
-    #------out(dict)----------------
+        conn.close()
     header = {"Content-Type": "application/json"}
-    res_1 = HTTPResponse(status=200, body=teachers_list, headers=header)
-    return res_1
+    res_4 = HTTPResponse(status=200, body=output_json, headers=header)
+    result_json=json.dumps(output_json,ensure_ascii=False)
+    print(result_json)
+    #output_1 = {"message": "OK"}
+    #------out(dict)----------------
+    #header = {"Content-Type": "application/json"}
+    #res_1 = HTTPResponse(status=200, body=output_1, headers=header)
+    return {result_json}
 #------------
 @route("/get/questions",method="GET")
 def get_2():
